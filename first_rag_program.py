28ffc67ec8159c5e9257ff1b86462e54405c8703
m5071106 <94752255+m5071106@users.noreply.github.com>
2024-06-26 19:14:53 +0900

    add question

--- first_rag_program.py
+++ first_rag_program.py
@@ -49,5 +49,7 @@ if __name__ == '__main__':
     # 質問文
     question1 = "株式会社ABCの開発を行う事業部は？"
     question2 = "株式会社ABCの運用を行う事業部は？"
+    question3 = "売上を管理するシステムは？"
     rag_sample(question1, documents)
     rag_sample(question2, documents)
+    rag_sample(question3, documents)
